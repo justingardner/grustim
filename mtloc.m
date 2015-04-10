8ca58da1e592d16c02d43986305f08ea694345a3
dbirman <danbirman@gmail.com>
2015-04-10 12:51:34 -0700

    Pushing dan's mtlocalizer code

---
+++ mtloc.m
@@ -0,0 +1,351 @@
+% mtloc.m
+%
+%        $Id: mtloc.m,v 1.6 2009/02/02 22:54:37 justin Exp $
+%      usage: mtloc
+%         by: justin gardner, dan birman
+%       date: 09/07/06, 04/?/15
+%  copyright: (c) 2006 Justin Gardner (GPL see mgl/COPYING)
+%    purpose: motion localizer task
+%
+function myscreen = mtloc(type,TR)
+
+% check arguments
+if ~any(nargin == [0 1 2])
+  help dotslocnew
+  return
+end
+
+if ~exist('type','var'),type = '0%';end
+if ~any(strcmp(type,{'static','0%'}))
+  disp(sprintf('(dotslocnew) Unknown type: %s',type));
+  return
+end
+
+% initalize the screen
+% myscreen.autoCloseScreen = 1;
+% myscreen.displayname = 'projector';
+myscreen.background = 'black';
+myscreen = initScreen(myscreen);
+
+% set the first task to be the fixation staircase task
+clear global fixStimulus;
+[task{1} myscreen] = fixStairInitTask(myscreen);
+
+num = round(12/TR);
+% a top-up period of the same direction
+task{2}{1}.seglen =     [repmat(TR,1,num) TR*num-.1];
+task{2}{1}.synchToVol = [0 0 0 0 0 0 0 0 0 0 0 0 1];
+task{2}{1}.parameter.coherence = 1;
+task{2}{1}.random = 1;
+task{2}{1}.numTrials = 11;
+task{2}{1}.fudgeLastVolume = 1;
+task{2}{1}.waitForBacktick = 1;
+
+% initialize our task
+for phaseNum = 1:length(task{2})
+  [task{2}{phaseNum} myscreen] = initTask(task{2}{phaseNum},myscreen,@startSegmentCallback,@updateScreenCallback);
+end
+
+% init the stimulus
+clear global stimulus;
+global stimulus;
+myscreen = initStimulus('stimulus',myscreen);
+stimulus.dots.type = 'Opticflow';
+stimulus = initDots(stimulus,myscreen);
+stimulus.type = type;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% run the eye calibration
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+myscreen = eyeCalibDisp(myscreen);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main display loop
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+phaseNum = 1;
+while (phaseNum <= length(task{2})) && ~myscreen.userHitEsc
+  % update the dots
+  [task{2} myscreen phaseNum] = updateTask(task{2},myscreen,phaseNum);
+  % update the fixation task
+  [task{1} myscreen] = updateTask(task{1},myscreen,1);
+  % flip screen
+  myscreen = tickScreen(myscreen,task);
+end
+
+% if we got here, we are at the end of the experiment
+myscreen = endTask(myscreen,task);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function that gets called at the start of each segment
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [task myscreen] = startSegmentCallback(task, myscreen)
+
+global stimulus;
+if (task.thistrial.thisseg < length(task.seglen))
+  stimulus.coherence = task.thistrial.coherence;
+  % set speed
+  stimulus.dots = feval(sprintf('setDotsSpeed%s',stimulus.dots.type),stimulus.dots,stimulus.speed,myscreen);
+  stimulus.dots = feval(sprintf('setDotsDir%s',stimulus.dots.type),stimulus.dots,2*mod(task.thistrial.thisseg,2)-1,myscreen);
+else
+  stimulus.coherence = 0;
+  if strcmp(stimulus.type,'static')
+    stimulus.dots = feval(sprintf('setDotsSpeed%s',stimulus.dots.type),stimulus.dots,0,myscreen);
+  end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function that gets called to draw the stimulus each frame
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [task myscreen] = updateScreenCallback(task, myscreen)
+
+global stimulus
+mglClearScreen;
+
+% update the dots
+stimulus.dots = feval(sprintf('updateDots%s',stimulus.dots.type),stimulus.dots,stimulus.coherence,myscreen);
+
+% draw the dots
+if stimulus.dots.mask,mglStencilSelect(1);end
+%mglPoints2(stimulus.dots.x(stimulus.dots.color==1),stimulus.dots.y(stimulus.dots.color==1),stimulus.dots.dotsize,[1 1 1]);
+%mglPoints2(stimulus.dots.x(stimulus.dots.color==0),stimulus.dots.y(stimulus.dots.color==0),stimulus.dots.dotsize,[0 0 0]);
+mglPoints2(stimulus.dots.x,stimulus.dots.y,stimulus.dots.dotsize,[1 1 1]);
+if stimulus.dots.mask,mglStencilSelect(0);end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function to init the dot stimulus
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function stimulus = initDots(stimulus,myscreen)
+
+stimulus.speed = 4;
+% convert the passed in parameters to real units
+if ~isfield(stimulus,'dots') || ~isfield(stimulus.dots,'rmax'), stimulus.dots.rmax = min(myscreen.imageWidth,myscreen.imageHeight);,end
+if ~isfield(stimulus.dots,'xcenter'), stimulus.dots.xcenter = 0;,end
+if ~isfield(stimulus.dots,'ycenter'), stimulus.dots.ycenter = 0;,end
+if ~isfield(stimulus.dots,'dotsize'), stimulus.dots.dotsize = 4;,end
+if ~isfield(stimulus.dots,'density'), stimulus.dots.density = 5;,end
+if ~isfield(stimulus.dots,'coherence'), stimulus.dots.coherence = 1;,end
+if ~isfield(stimulus.dots,'speed'), stimulus.dots.speed = stimulus.speed;,end
+if ~isfield(stimulus.dots,'dir'), stimulus.dots.dir = 0;,end
+if ~isfield(stimulus.dots,'mask'), stimulus.dots.mask = 1;,end
+
+% update the dots
+stimulus.dots = feval(sprintf('initDots%s',stimulus.dots.type),stimulus.dots,myscreen);
+
+% set color
+stimulus.dots.color = ones(stimulus.dots.n,1);
+%stimulus.dots.color(rand(1,stimulus.dots.n)>0.5) = 1;
+
+% create stencil
+if stimulus.dots.mask
+  mglClearScreen;
+  mglStencilCreateBegin(1);
+  % and draw that oval
+  mglGluDisk(stimulus.dots.xcenter,stimulus.dots.ycenter,[stimulus.dots.rmax stimulus.dots.rmax]/2,[1 1 1],60);
+  mglStencilCreateEnd;
+  mglClearScreen;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% set the dots speed
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dots = setDotsSpeedLinear(dots,speed,myscreen)
+
+% get the step size
+dots.speed = speed;
+dots.stepsize = speed/myscreen.framesPerSecond;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% set the dots direction
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dots = setDotsDirectionLinear(dots,direction,myscreen)
+
+% get the step size
+dots.dir = direction;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step dots for linear
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dots = updateDotsLinear(dots,coherence,myscreen)
+
+% get the dots step
+dots.xstep = cos(pi*dots.dir/180)*dots.stepsize;
+dots.ystep = sin(pi*dots.dir/180)*dots.stepsize;
+
+% pick a random set of dots
+dots.coherent = rand(1,dots.n) < coherence;
+
+% now move those dots in the right direction
+dots.x(dots.coherent) = dots.x(dots.coherent)+dots.xstep;
+dots.y(dots.coherent) = dots.y(dots.coherent)+dots.ystep;
+
+% randomwalk rule
+thisdir = rand(1,sum(~dots.coherent))*2*pi;
+dots.x(~dots.coherent) = dots.x(~dots.coherent)+cos(thisdir)*dots.stepsize;
+dots.y(~dots.coherent) = dots.y(~dots.coherent)+sin(thisdir)*dots.stepsize;
+
+% movshon noise
+%dots.x(~dots.coherent) = rand(1,sum(~dots.coherent))*dots.width;
+%dots.y(~dots.coherent) = rand(1,sum(~dots.coherent))*dots.height;
+
+% make sure we haven't gone off the patch
+% do the dots separately for left and right hand side
+dots.x(dots.x < dots.xmin) = dots.x(dots.x < dots.xmin)+dots.width;
+dots.x(dots.x > dots.xmax) = dots.x(dots.x > dots.xmax)-dots.width;
+dots.y(dots.y < dots.ymin) = dots.y(dots.y < dots.ymin)+dots.height;
+dots.y(dots.y > dots.ymax) = dots.y(dots.y > dots.ymax)-dots.height;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% step dots for opticflow
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dots = updateDotsOpticflow(dots,coherence,myscreen)
+
+% get the coherent and incoherent dots
+%if (dots.coherency ~= coherence)
+  dots.incoherent = rand(1,dots.n) > coherence;
+  dots.incoherentn = sum(dots.incoherent);
+  dots.coherent = ~dots.incoherent;
+  dots.coherency = coherence;
+  % generate a random transformation matrix for each incoherent point
+  dots.randT = rand(3,dots.incoherentn)-0.5;
+  % and normalize the transformation to have the same length
+  % (i.e. speed) as the real transformation matrix
+  dots.randT = sqrt(sum(dots.T.^2))*dots.randT./([1 1 1]'*sqrt(sum(dots.randT.^2)));
+%end
+
+% update relative position of dots in 3-space to observer
+dots.X(dots.coherent) = dots.X(dots.coherent)-dots.T(1);
+dots.Y(dots.coherent) = dots.Y(dots.coherent)-dots.T(2);
+dots.Z(dots.coherent) = dots.Z(dots.coherent)-dots.T(3);
+
+% now move the incoherent points according to the random trasnformation
+dots.X(dots.incoherent) = dots.X(dots.incoherent)-dots.randT(1,:);
+dots.Y(dots.incoherent) = dots.Y(dots.incoherent)-dots.randT(2,:);
+dots.Z(dots.incoherent) = dots.Z(dots.incoherent)-dots.randT(3,:);
+
+% get all points that have fallen off the screen
+offscreen = dots.Z<dots.minZ;
+
+% and put them at the furthest distance
+dots.Z(offscreen) = dots.maxZ;
+
+% get all points that have fallen out of view
+offscreen = dots.Z>dots.maxZ;
+% and move them to the front plane
+dots.Z(offscreen) = dots.minZ;
+
+% put points fallen off the X edge back
+offscreen = dots.X < -dots.maxX;
+dots.X(offscreen) = dots.X(offscreen)+2*dots.maxX;
+offscreen = dots.X > dots.maxX;
+dots.X(offscreen) = dots.X(offscreen)-2*dots.maxX;
+
+% put points fallen off the Y edge back
+offscreen = dots.Y < -dots.maxY;
+dots.Y(offscreen) = dots.Y(offscreen)+2*dots.maxY;
+offscreen = dots.Y > dots.maxY;
+dots.Y(offscreen) = dots.Y(offscreen)-2*dots.maxY;
+
+% project on to screen
+dots.xproj = dots.f*dots.X./dots.Z;
+dots.yproj = dots.f*dots.Y./dots.Z;
+
+% stuff to compute median speed
+dots.oldx = dots.x;
+dots.oldy = dots.y;
+
+% get actual screen coordinates
+dots.x = dots.xproj*myscreen.imageWidth;
+dots.y = dots.yproj*myscreen.imageHeight;
+
+%medianSpeed = median(sqrt((dots.oldx-dots.x).^2+(dots.oldy-dots.y).^2)*myscreen.framesPerSecond);
+%minSpeed = min(sqrt((dots.oldx-dots.x).^2+(dots.oldy-dots.y).^2)*myscreen.framesPerSecond);
+%disp(sprintf('min: %f median: %f',minSpeed,medianSpeed));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% create dots for linear2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dots = initDotsLinear(dots,myscreen)
+
+% actually a square patch of dots that get stenciled
+% so calculate width and height
+dots.width = dots.rmax*2;
+dots.height = dots.rmax*2;
+
+% get the number of dots
+dots.n = round(dots.width*dots.height*dots.density);
+
+% get max and min points for dots
+dots.xmin = -dots.width/2;
+dots.xmax = dots.width/2;
+dots.ymin = -dots.height/2;
+dots.ymax = dots.height/2;
+
+% get initial position
+dots.x = rand(1,dots.n)*dots.width;
+dots.y = rand(1,dots.n)*dots.height;
+
+% get the step size
+dots.stepsize = dots.speed/myscreen.framesPerSecond;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% set the dots speed
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dots = setDotsSpeedOpticflow(dots,speed,myscreen)
+
+% get the step size
+dots.speed = speed;
+dots.T = [0 0 dots.speed/myscreen.framesPerSecond];
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% set the dots direction
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dots = setDotsDirOpticflow(dots,direction,myscreen)
+
+% get the step size
+dots.T = [0 0 direction*dots.speed/myscreen.framesPerSecond];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% create dots for optic flow
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dots = initDotsOpticflow(dots,myscreen)
+
+% focal length to projection plane
+% projection plane is defined to be 
+% 1 unit wide and high, so with 
+% this focal length, we are looking at
+% a view of the world with a 90 deg fov
+dots.f = .5;
+
+% translation and rotation matrices
+dots.T = [0 0 dots.speed/myscreen.framesPerSecond];
+dots.R = [0 0 0];
+
+% maximum depth of points
+dots.maxZ = 10;dots.minZ = dots.f;
+dots.maxX = 10;
+dots.maxY = 10;
+
+% make a brick of points
+dots.n = round(myscreen.imageWidth*myscreen.imageHeight*dots.density);
+
+% initial position of dots
+dots.X = 2*dots.maxX*rand(1,dots.n)-dots.maxX;
+dots.Y = 2*dots.maxY*rand(1,dots.n)-dots.maxY;
+dots.Z = (dots.maxZ-dots.minZ)*rand(1,dots.n)+dots.minZ;
+
+% get projection on to plane
+dots.xproj = dots.f*dots.X./dots.Z;
+dots.yproj = dots.f*dots.Y./dots.Z;
+
+% put into screen coordinates
+dots.x = dots.xproj*myscreen.imageWidth;
+dots.y = dots.yproj*myscreen.imageHeight;
+
+% set incoherent dots to 0
+dots.coherency = 1;
+dots.incoherent = rand(1,dots.n) > dots.coherency;
+dots.incoherentn = sum(dots.incoherent);
+dots.coherent = ~dots.incoherent;
+
+dots.randT = zeros(3,dots.incoherentn);
