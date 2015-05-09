2134b72a2fe4e2fd64279afe1ff88a9ecd9ef705
dbirman <danbirman@gmail.com>
2015-05-08 17:22:28 -0700

    Prep for Scanning

--- EXP_dbirman_cohcon_2015.m
+++ EXP_dbirman_cohcon_2015.m
@@ -1,8 +1,7 @@
 %% Intro
 
 % This script runs the coherentContrast experiment
-% This revision is for March 2015, data collection for Dan's FYP project.
-
+% This revision is for May 2015, data collection for Dan's FYP project.
 
 %% Retinotopy + MTLoc
 
@@ -24,33 +23,65 @@
 % % Retinotopy BARS x4:
 % mglRetinotopy('displayName=fMRIproj32','fixedRandom=1','stimulusPeriod=24','stepsPerCycle',17,'blanks=3','doEyeCalib=0','bars=1');
 
-%% Unattended Task
-
-% The 32ch coil should be installed.
-
-% unattended x4-6
-% cohcon('unattended=1','plots=0','scan=1');
-
-% After running the unattended task collect an in-plane anatomical OR the
-% canonical anatomical;
-
 %% MT Localizer
 
 % The 32ch coil should be installed.
-disp(sprintf('Running MT Localizer.'));
-while ~strcmp(input('Have you prepped the mux8 script? [y] ','s'),'y')
-end
-
-% mtLoc x2
-% mtloc('0%',1.4);
-mtloc('0%',.75);
+% disp(sprintf('Running MT Localizer.'));
+% while ~strcmp(input('Have you prepped the mux8 script? [y] ','s'),'y')
+% end
+% 
+% % mtLoc x2
+% % mtloc('0%',1.4);
+% mtloc('0%',.75);
 
 %% Full Experiment
 
 % The 32ch coil should be installed.
 
-% exp script
+% Hi Steeve. Did you run the in plane anatomy yet? Do that. Also run the
+% CAL sequence!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% IN PLANE ANATOMY FIRST!! %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Then, run these first:
+% You should be using the 2.5mm mux8 sequence. Total volumes = 840 (7:00).
+%%
+cohcon('plots=0','scan=1','nocatch=1');
+%%
+cohcon('plots=0','scan=1','nocatch=1');
+%%
+cohcon('plots=0','scan=1','nocatch=1');
+%%
+cohcon('plots=0','scan=1','nocatch=1');
+
+% Then run four of these:
+%%
+cohcon('plots=0','scan=1');
+%%
+cohcon('plots=0','scan=1');
+%%
+cohcon('plots=0','scan=1');
+%%
 cohcon('plots=0','scan=1');
 
-% After running the experiment collect an in-plane anatomical OR the
-% canonical anatomical;
+% Then run four more of those first ones:
+%%
+cohcon('plots=0','scan=1','nocatch=1');
+%%
+cohcon('plots=0','scan=1','nocatch=1');
+%%
+cohcon('plots=0','scan=1','nocatch=1');
+%%
+cohcon('plots=0','scan=1','nocatch=1');
+
+% Then two more of these:
+%%
+cohcon('plots=0','scan=1');
+%%
+cohcon('plots=0','scan=1');
+
+% Now just run as many of these as possible until I collapse and die.
+% Please give me breaks once in a while.
+%%
+cohcon('plots=0','scan=1','nocatch=1');
