97c815afb96701a1c2799f18b9c9777e224d9483
minyounglee <mlee17@stanford.edu>
2016-07-06 20:52:56 -0700

    changed stim size

--- attentionComp.m
+++ attentionComp.m
@@ -1,7 +1,10 @@
-function myscreen = attentionComp()
-
-%task
-%localizer
+% attentionComp.m
+%
+%      usage: myscreen = attentionComp()
+%         by: minyoung lee
+%       date: 07/01/2016
+%    purpose: biased competition experiment
+%
 
 % Contrasts
 % Low/High 12.5/50%
@@ -9,15 +12,17 @@ function myscreen = attentionComp()
 % Orientations
 % Vertical/CW to vertical => staircased
 
+function myscreen = attentionComp()
+
 global stimulus
-stimulus.width = 1.2; % gabor
+stimulus.width = 6; % gabor
 stimulus.contrastLow = .125;
 stimulus.contrastHigh = .5;
-stimulus.sf = 4.8;
+stimulus.sf = 1.8;
 stimulus.orientation = 90;
-% angles  (1->4 clockwise from top)`
-% loc1: pi/3 (60)  loc2: pi/6 (30)  loc3: pi*4/3 (240) loc4: pi*7/6 (210)
-stimulus.angles = [pi/3 pi/6 pi*4/3 pi*7/6];
+% angles  (1->4 clockwise from top)
+% loc1: 60  loc2: 30  loc3: 240 loc4: 210
+stimulus.angles = [65 25 245 205] * pi /180;
 stimulus.locations = 1:length(stimulus.angles);
 stimulus.radius = 6;
 stimulus.upperLoc = [1 2];
@@ -30,12 +35,10 @@ stimulus.cueColor = [1 1 1];
 stimulus.interval = [2 4];
 
 %set parameters of staircase
-stimulus.initialThreshold = 10; 
-stimulus.stepsize = .25;
+stimulus.initialThreshold = 5;
+stimulus.stepsize = .05;
 stimulus.minThreshold = 0;
 stimulus.maxThreshold = 45;
-% stimulus.lowCon = stair;
-% stimulus.highCon = stair;
 
 % initalize the screen
 myscreen.background = 0.5;
@@ -45,19 +48,25 @@ myscreen = initScreen(myscreen);
 % set up task
 %%%%%%%%%%%%%%%%%%%%%
 
-task{1}.waitForBacktick = 1;
-task{1}.segmin = [1 0.7 0.3 0.7 1.5 1.5];
-task{1}.segmax = [1 0.7 0.3 0.7 1.5 5];
+task{1}.waitForBacktick = 0;
+% task{1}.waitForBacktick = 1;
+task{1}.segmin = [1 3 0.3 0.7 1.5 1.5];
+task{1}.segmax = [1 3 0.3 0.7 1.5 5];
 task{1}.getResponse = [0 0 0 0 1 0];
 % task{1}.synchToVol = [0 0 0 0 0 1];
 task{1}.random = 1;
 
 % parameters
-
+task{1}.parameter.targetContrast = [1 2]; %low/high
 task{1}.randVars.uniform.targetLoc = stimulus.locations;
-task{1}.randVars.uniform.targetContrast = [1 2]; %low/high
+% task{1}.randVars.uniform.targetContrast = [1 2]; %low/high
 task{1}.randVars.block.unattendedStim = [-1 1 2]; %low alone/ high alone/ both
 task{1}.randVars.block.unattendedStimPos = [1 2];
+
+task{1}.randVars.calculated.attendContrast = nan;
+task{1}.randVars.calculated.attendAway = nan;
+task{1}.randVars.calculated.attendedPair= {nan};
+task{1}.randVars.calculated.unattendedPair = {nan};
 
 % initialize the task
 for phaseNum = 1:length(task)
@@ -68,7 +77,7 @@ end
 myscreen = initStimulus('stimulus',myscreen);
 
 % to initialize the stimulus for your experiment.
-stimulus = myInitStimulus(stimulus,myscreen);
+stimulus = initGabor(stimulus,myscreen);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % run the eye calibration
@@ -89,7 +98,7 @@ end
 % if we got here, we are at the end of the experiment
 myscreen = endTask(myscreen,task);
 
-%%
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % function that gets called at the start of each segment
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -112,11 +121,13 @@ if task.thistrial.thisseg == 1
         task.thistrial.tex{attendedPair(task.thistrial.targetLoc ~= attendedPair)} = stimulus.tex(2);
         task.thistrial.contrast{task.thistrial.targetLoc} = 'low';
         task.thistrial.contrast{attendedPair(task.thistrial.targetLoc ~= attendedPair)} = 'high';
+        task.thistrial.attendContrast = 1;
     else
         task.thistrial.tex{task.thistrial.targetLoc} = stimulus.tex(2);
         task.thistrial.tex{attendedPair(task.thistrial.targetLoc ~= attendedPair)} = stimulus.tex(1);
         task.thistrial.contrast{task.thistrial.targetLoc} = 'high';
-        task.thistrial.contrast{attendedPair(task.thistrial.targetLoc ~= attendedPair)} = 'low';     
+        task.thistrial.contrast{attendedPair(task.thistrial.targetLoc ~= attendedPair)} = 'low';
+        task.thistrial.attendContrast = 2;
     end
         
     if task.thistrial.unattendedStimPos == 1
@@ -132,9 +143,13 @@ if task.thistrial.thisseg == 1
     if task.thistrial.unattendedStim == -1
         task.thistrial.tex{unattendedPair(high)} = nan;
         task.thistrial.contrast{unattendedPair(high)} = nan;
+        task.thistrial.attendAway = -1;
     elseif task.thistrial.unattendedStim == 1
         task.thistrial.tex{unattendedPair(low)} = nan;
         task.thistrial.contrast{unattendedPair(low)} = nan;
+        task.thistrial.attendAway = 1;
+    else
+        task.thistrial.attendAway = 2;
     end
 
     task.thistrial.attendedPair = attendedPair;
@@ -168,9 +183,6 @@ function [task myscreen] = screenUpdateCallback(task, myscreen)
 global stimulus
 mglClearScreen;
 
-%draw fixation cross
-mglFixationCross(stimulus.fixWidth,1.5,stimulus.fixColor);
-
 % draw stim, 2nd/4th seg
 if any(task.thistrial.thisseg == stimulus.interval)  
     for i = 1:length(stimulus.locations)
@@ -185,7 +197,12 @@ if task.thistrial.thisseg ~= 6 % draw the cue
         stimulus.cueLines.x1(task.thistrial.targetLoc), stimulus.cueLines.y1(task.thistrial.targetLoc), 1.5, stimulus.cueColor);    
 end
 
+%draw fixation cross
+mglFixationCross(stimulus.fixWidth,1.5,stimulus.fixColor);
 
+% for i = 1:length(stimulus.locations)
+%      mglLines2(stimulus.refLines.x1{i},stimulus.refLines.y1{i},stimulus.refLines.x2{i},stimulus.refLines.y2{i},1,[1 1 1]);
+% end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %    responseCallback    %
@@ -202,37 +219,40 @@ if task.thistrial.whichInterval(task.thistrial.targetLoc) == task.thistrial.whic
     stimulus.cueColor = [0 1 0];
     %update staircase
     stimulus.stair{task.thistrial.targetContrast} = upDownStaircase(stimulus.stair{task.thistrial.targetContrast},1);
-    disp(sprintf('(ExptName) target: %s contrast %0.4f correct', task.thistrial.contrast{task.thistrial.targetLoc},stimulus.stair{task.thistrial.targetContrast}.threshold));
+    disp(sprintf('(ExptName) %i: %s contrast %0.4f correct', task.trialnum, task.thistrial.contrast{task.thistrial.targetLoc},stimulus.stair{task.thistrial.targetContrast}.threshold));
 else
     % incorrect
     task.thistrial.correct = false;
     stimulus.cueColor = [1 0 0];
     %update staircase
     stimulus.stair{task.thistrial.targetContrast} = upDownStaircase(stimulus.stair{task.thistrial.targetContrast},0);
-    disp(sprintf('(ExptName) target: %s contrast %0.4f incorrect', task.thistrial.contrast{task.thistrial.targetLoc}, stimulus.stair{task.thistrial.targetContrast}.threshold));
+    disp(sprintf('(ExptName) %i: %s contrast %0.4f incorrect', task.trialnum, task.thistrial.contrast{task.thistrial.targetLoc}, stimulus.stair{task.thistrial.targetContrast}.threshold));
 end
 end
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % function to init the stimulus
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function stimulus = myInitStimulus(stimulus,myscreen)
+function stimulus = initGabor(stimulus,myscreen)
 
 % compute the grating
 grating = mglMakeGrating(stimulus.width, stimulus.width, stimulus.sf, stimulus.orientation,0);
-gaussian = mglMakeGaussian(stimulus.width, stimulus.width, stimulus.width/8, stimulus.width/8); %sd?????
+gaussian = mglMakeGaussian(stimulus.width, stimulus.width,stimulus.width/10, stimulus.width/10); %sd?????
 
-gaborLow = 255*(stimulus.contrastLow*grating.*gaussian+1)/2;
-gaborHigh = 255*(stimulus.contrastHigh*grating.*gaussian+1)/2;
+gaborLow(:,:,1) = 255*(grating+1)/2;
+gaborLow(:,:,2) = 255*(grating+1)/2;
+gaborLow(:,:,3) = 255*(grating+1)/2;
+gaborLow(:,:,4) =255*(stimulus.contrastLow*gaussian);
+
+gaborHigh(:,:,1) = 255*(grating+1)/2;
+gaborHigh(:,:,2) = 255*(grating+1)/2;
+gaborHigh(:,:,3) = 255*(grating+1)/2;
+gaborHigh(:,:,4) =255*(stimulus.contrastHigh*gaussian);
 
 %create texture
 stimulus.tex(1) = mglCreateTexture(gaborLow);
 stimulus.tex(2) = mglCreateTexture(gaborHigh);
 
-%stim loc
-% angles  (1->4 clockwise)
-% loc1: pi/3 (60)  loc2: pi/6 (30)  loc3: pi*4/3 (240) loc4: pi*7/6 (210)
 %cue lines / stimulus centers
 for i = 1:length(stimulus.angles)
       % starting point of lines
@@ -242,6 +262,21 @@ for i = 1:length(stimulus.angles)
       
       %stim centers
       [stimulus.x(i), stimulus.y(i)] = pol2cart(stimulus.angles(i),stimulus.radius);
+      
+        % circular reference lines
+    stimulus.refLines.x1{i} = [];
+    stimulus.refLines.y1{i} = [];
+    stimulus.refLines.x2{i} = [];
+    stimulus.refLines.y2{i} = [];
+  d = 0:1:360;
+  for dIndex = 1:length(d)-1
+    stimulus.refLines.x1{i} = [stimulus.refLines.x1{i} stimulus.x(i)+stimulus.width/2*cos(d2r(d(dIndex)))];
+    stimulus.refLines.y1{i} = [stimulus.refLines.y1{i} stimulus.y(i)+stimulus.width/2*sin(d2r(d(dIndex)))];
+    stimulus.refLines.x2{i} = [stimulus.refLines.x2{i} stimulus.x(i)+stimulus.width/2*cos(d2r(d(dIndex+1)))];
+    stimulus.refLines.y2{i} = [stimulus.refLines.y2{i} stimulus.y(i)+stimulus.width/2*sin(d2r(d(dIndex+1)))];
+  end
+      
+      
 end
 
 % now create staircases for each orientation
