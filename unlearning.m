f646f43c0094a0a6b6d98fa3244bc7c93ff08453
Dan Birman <danbirman@gmail.com>
2017-01-19 14:56:25 -0800

    Unlearning functional

--- unlearning.m
+++ unlearning.m
@@ -56,7 +56,7 @@ stimulus.cur{end}.N = 4;
 stimulus.cur{end}.K = 2;
 stimulus.cur{end}.angle = 30;
 stimulus.cur{end}.num = 360/stimulus.cur{end}.angle;
-stimulus.cur{end}.buffer = 5; % buffer is used to stencil over the wedges
+stimulus.cur{end}.buffer = 10; % buffer is used to stencil over the wedges
 stimulus.cur{end}.isize = 1;
 stimulus.cur{end}.osize = 10;
 
@@ -160,7 +160,7 @@ task{1}{1}.getResponse = zeros(size(task{1}{1}.segmin)); task{1}{1}.getResponse(
 task{1}{1}.numTrials = 50;
 task{1}{1}.random = 1;
 task{1}{1}.parameter.match = [0 1];
-task{1}{1}.parameter.count = [0 0 0 0 0 0 1 2];
+task{1}{1}.parameter.count = [0 0 0 0 1 2];
 
 if stimulus.scan
     task{1}{1}.synchToVol(stimulus.seg.ITI) = 1;
@@ -219,23 +219,27 @@ end
 mglClearScreen(0.5);
 mglTextSet([],32,stimulus.colors.white);
 % get count
-count = 0;
+count = 0; got=false;
 while true
-    mglTextDraw('Report your count: 0',[0 0]);
+    mglClearScreen(0.5);
+    mglTextDraw(sprintf('Report your count: %i',count),[0 0]);
     mglFlush
     keys = mglGetKeys;
-    if keys(19)
+    if keys(19) && ~got
+        got = true;
         count = count+1;
+    elseif ~keys(19)
+        got = false;
     end
     if keys(20)
         break;
     end
 end
-mglTextDraw(sprintf('True count: %i',task.thistrial.cumcount),[0 2]);
+mglTextDraw(sprintf('True count: %i',task{1}{1}.thistrial.cumcount),[0 2]);
 myscreen.flushMode = 1;
 
 stimulus.count = count;
-stimulus.truecount = task.thistrial.cumcount;
+stimulus.truecount = task{1}{1}.thistrial.cumcount;
 stimulus.off = abs(stimulus.count-stimulus.truecount);
 disp(sprintf('(unlearn) Subject was off by: %i. True: %i Report: %i',stimulus.off,stimulus.truecount,stimulus.count));
 
@@ -290,6 +294,12 @@ for i = 1:stimulus.cur_.num
             stimulus.live.rings2(:,i) = stimulus.patterns(notpatterns(randi(length(notpatterns))),:)';
         end
     end
+end
+
+if task.thistrial.count==1
+    stimulus.live.rings1(randi(size(stimulus.live.rings1,1)),randi(size(stimulus.live.rings1`21212,2))) = 2;
+elseif task.thistrial.count==2
+    stimulus.live.rings2(randi(size(stimulus.live.rings2,1)),randi(size(stimulus.live.rings2,2))) = 2;
 end
 
 opts = {'Non-match','Match'};
@@ -355,6 +365,8 @@ mglClearScreen(0.5);
 % % %     end
 % % % end
 
+partSize = (stimulus.cur_.osize-stimulus.cur_.isize) / stimulus.cur_.N;
+
 if stimulus.live.stim
     if task.thistrial.thisseg==stimulus.seg.stim1
         rings = stimulus.live.rings1;
@@ -363,15 +375,29 @@ if stimulus.live.stim
     end
     mglStencilSelect(1);
     % draw rings
-    
+    for rn = 1:stimulus.cur_.N
+        for si = 1:stimulus.cur_.num
+            if rings(rn,si)==2
+                col = [1 165/255 0];
+            else
+                col = rings(rn,si)*[1 1 1];
+            end
+            mglGluPartialDisk(0,0,(rn-1)*partSize+stimulus.cur_.isize,rn*partSize+stimulus.cur_.isize,si*stimulus.cur_.angle,stimulus.cur_.angle,col);
+        end
+    end
     % revert stencil
     mglStencilSelect(0);
 end
 
 if stimulus.live.fix
 %      cover
-    mglFillOval(0,0,[1 1],0.5);
-    upFix(stimulus);
+    if stimulus.live.resp==1
+        mglTextSet([],32,stimulus.live.fixColor);
+        mglTextDraw(stimulus.live.respText,[0 0]);
+    else
+        mglFillOval(0,0,[1 1],0.5);
+        upFix(stimulus);
+    end
 end
 
 % if stimulus.live.triggerWaiting
@@ -393,12 +419,6 @@ end
 %     end
 % end
 
-function upGrating(stimulus,task)
-
-% phaseNum = floor(length(stimulus.grating.phases)*rem(mglGetSecs(task.thistrial.trialstart)*stimulus.grating.tf,1)+1);
-mglBltTexture(stimulus.tex(stimulus.live.phaseNum),[stimulus.live.x stimulus.live.y 6],0,0,stimulus.live.dir*180/pi+90); % we add 90 so that it's aligned with the motion
-mglBltTexture(stimulus.mask,[stimulus.live.x stimulus.live.y 6 6],0,0,stimulus.live.dir*180/pi+90);
-
 function upFix(stimulus)
 %%
 % for this experiment use a circle to indicate where participants can
@@ -406,20 +426,6 @@ function upFix(stimulus)
 % poisitioning
 % mglGluAnnulus(0,0,1.5,1.55,stimulus.live.fixColor,64);
 mglFixationCross(1,1,stimulus.live.fixColor);
-
-function stimulus = upDots(stimulus,myscreen)
-
-stimulus.dots{stimulus.dot} = updateDotsRadial(stimulus.dots{stimulus.dot},stimulus.live.coherence,myscreen,true);
-
-mglPoints2(stimulus.dots{stimulus.dot}.x+stimulus.live.x,stimulus.dots{stimulus.dot}.y+stimulus.live.y,...
-    stimulus.dots{stimulus.dot}.dotsize,stimulus.live.dotColor);
-
-mglBltTexture(stimulus.mask,[stimulus.live.x stimulus.live.y 6 6],0,0,stimulus.live.dir*180/pi+90);
-
-% function stimulus = upDotsInc(stimulus,myscreen)
-% stimulus.idots = updateDotsRadial(stimulus.idots,0,myscreen,true);
-% mglPoints2(stimulus.idots.x,stimulus.idots.y,...
-%     stimulus.idots.dotsize,stimulus.live.dotColor);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% Called When a Response Occurs %%%%%%%%%%%%%%%%%%%%
@@ -431,19 +437,18 @@ global stimulus
 
 if stimulus.dead, return; end
 responseText = {'Incorrect','Correct'};
+respText = {'-1','+5'};
 sideText = {'Left','Right'};
 fixColors = {stimulus.colors.red,stimulus.colors.green};
     
-responses = [1 0 2];
 if any(task.thistrial.whichButton == stimulus.responseKeys)
     if task.thistrial.gotResponse == 0
-        task.thistrial.correct = task.thistrial.whichButton == stimulus.responseKeys(responses(task.thistrial.rotation+2));
+        task.thistrial.correct = task.thistrial.whichButton == stimulus.responseKeys(task.thistrial.match+1);
         disp(sprintf('Subject pressed %i: %s %s',task.thistrial.whichButton,sideText{task.thistrial.whichButton},responseText{task.thistrial.correct+1}));
         stimulus.live.fixColor = fixColors{task.thistrial.correct+1};
-        stimulus.staircase{stimulus.feature} = doStaircase('update',stimulus.staircase{stimulus.feature},task.thistrial.correct);
         stimulus.live.resp = 1;
-        stimulus.live.dots = 0;
-        stimulus.live.fix=1;
+        stimulus.live.fix = 1;
+        stimulus.live.respText = respText{task.thistrial.correct+1};
     else
         disp(sprintf('(unlearn) Subject responded multiple times: %i',task.thistrial.gotResponse+1));
     end
@@ -453,267 +458,74 @@ end
 %%                              HELPER FUNCTIONS                           %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%
-%    initStaircase     %
-%%%%%%%%%%%%%%%%%%%%%%%%
-function stimulus = initStaircase(stimulus)
-%%
-stimulus.staircase = {};
-stimulus.staircase{1} = doStaircase('init','upDown',...
-        'initialThreshold',0.25,... % radians of rotation (
-        'initialStepsize',0.05,...
-        'minThreshold=0.001','maxThreshold=0.35','stepRule','pest',...
-        'nTrials=50','maxStepsize=0.1','minStepsize=0.001');
-stimulus.staircase{2} = stimulus.staircase{1};
-
-function stimulus = resetStair(stimulus)
-
-for i = 1:2
-    if doStaircase('stop',stimulus.staircase{i})
-        disp('(unlearn) Initializing new staircase...');
-        stimulus.staircase{i}(end+1) = doStaircase('init',stimulus.staircase{i}(end));
-    end
-end
-
 %%%%%%%%%%%%%%%%%%%%%%%
 %    dispInfo    %
 %%%%%%%%%%%%%%%%%%%%%%%
 function dispInfo(stimulus)
 %%
 
-if ~stimulus.localizer && ~stimulus.staircasing
-    disp(sprintf('Participant %s has earned $%2.2f',mglGetSID,stimulus.run.points/100));
-end
-% load the luminance table
-% % % load(myscreen.calibFullFilename)
-% % % luminance = interp1(calib.tableCorrected.outputValues,calib.tableCorrected.luminance,0:1/255:255);
-if stimulus.staircasing
-    %%
-    notstaircase = stimulus.staircase;
-    thresholds = zeros(size(stimulus.run.stimLengths));
-    for i = 1:length(stimulus.staircase)
-        out = doStaircase('threshold',notstaircase{i},'type','weibull','dispFig=0');
-        thresholds(i) = out.threshold;
-    end
-    % reorganize into matrix
-    stimCons = unique(stimulus.run.stimCon);
-    stimCons = sort(stimCons);
-    stimLengths = unique(stimulus.run.stimLengths);
-    stimLengths = sort(stimLengths);
-    datamat = nan(length(stimCons),length(stimLengths),5);
-    for ci = 1:length(stimCons)
-        for li = 1:length(stimLengths)
-            idxs = logical((stimulus.run.stimLengths==stimLengths(li)) .* (stimulus.run.stimCon==stimCons(ci)));
-            datamat(ci,li,1:sum(idxs)) = thresholds(idxs);
-        end
-    end
-    if any(thresholds<0) || any(thresholds>1)
-        % remove errant thresholds
-        warning('should remove some thresholds...');
-    end
-    datamat(datamat>1) = NaN;
-    datamat(datamat<=0) = NaN;
-    %%
-    datamu = nanmean(datamat,3);
-    datamu(datamu==0) = NaN;
-    datamu = round((1-datamu)*255);
-    datasd = nanstd(datamat,[],3);
-    datasd(datasd==0) = NaN;
-    %% plot
-    cmap = brewermap(length(stimCons)+1,'Purples');
-    cmap = cmap(2:end,:);
-    figure, hold on
-    legs = {};
-    for i = 1:length(stimCons)
-        plot(stimLengths,datamu(i,:),'o','MarkerFaceColor',cmap(i,:),'MarkerEdgeColor',[1 1 1],'MarkerSize',10);
-        errbar(stimLengths,datamu(i,:),datasd(i,:),'-','Color',cmap(i,:));
-        legs{end+1} = sprintf('Stimulus luminance: %i/255',stimCons(i));
-    end
-    a = axis;
-    axis([50 100 0 a(4)]);
-    legend(legs)
-    xlabel('Stimulus length (ms)');
-    ylabel('Mask contrast at just noticeable difference (% luminance)');
-    drawPublishAxis
-elseif stimulus.localizer
-else
-%     perf = zeros(size(stimulus.istaircase));
-%     for i = 1:length(stimulus.istaircase)
-%         perf(i) = mean(stimulus.istaircase(i).response);
+% if ~stimulus.localizer && ~stimulus.staircasing
+%     disp(sprintf('Participant %s has earned $%2.2f',mglGetSID,stimulus.run.points/100));
+% end
+% % load the luminance table
+% % % % load(myscreen.calibFullFilename)
+% % % % luminance = interp1(calib.tableCorrected.outputValues,calib.tableCorrected.luminance,0:1/255:255);
+% if stimulus.staircasing
+%     %%
+%     notstaircase = stimulus.staircase;
+%     thresholds = zeros(size(stimulus.run.stimLengths));
+%     for i = 1:length(stimulus.staircase)
+%         out = doStaircase('threshold',notstaircase{i},'type','weibull','dispFig=0');
+%         thresholds(i) = out.threshold;
 %     end
-%     figure
-%     plot(1:length(perf),perf,'o','MarkerFaceColor',[0 0 0],'MarkerEdgeColor',[1 1 1]);
-%     set(gca,'XAxisTick',1:length(perf));
+%     % reorganize into matrix
+%     stimCons = unique(stimulus.run.stimCon);
+%     stimCons = sort(stimCons);
+%     stimLengths = unique(stimulus.run.stimLengths);
+%     stimLengths = sort(stimLengths);
+%     datamat = nan(length(stimCons),length(stimLengths),5);
+%     for ci = 1:length(stimCons)
+%         for li = 1:length(stimLengths)
+%             idxs = logical((stimulus.run.stimLengths==stimLengths(li)) .* (stimulus.run.stimCon==stimCons(ci)));
+%             datamat(ci,li,1:sum(idxs)) = thresholds(idxs);
+%         end
+%     end
+%     if any(thresholds<0) || any(thresholds>1)
+%         % remove errant thresholds
+%         warning('should remove some thresholds...');
+%     end
+%     datamat(datamat>1) = NaN;
+%     datamat(datamat<=0) = NaN;
+%     %%
+%     datamu = nanmean(datamat,3);
+%     datamu(datamu==0) = NaN;
+%     datamu = round((1-datamu)*255);
+%     datasd = nanstd(datamat,[],3);
+%     datasd(datasd==0) = NaN;
+%     %% plot
+%     cmap = brewermap(length(stimCons)+1,'Purples');
+%     cmap = cmap(2:end,:);
+%     figure, hold on
+%     legs = {};
+%     for i = 1:length(stimCons)
+%         plot(stimLengths,datamu(i,:),'o','MarkerFaceColor',cmap(i,:),'MarkerEdgeColor',[1 1 1],'MarkerSize',10);
+%         errbar(stimLengths,datamu(i,:),datasd(i,:),'-','Color',cmap(i,:));
+%         legs{end+1} = sprintf('Stimulus luminance: %i/255',stimCons(i));
+%     end
+%     a = axis;
+%     axis([50 100 0 a(4)]);
+%     legend(legs)
+%     xlabel('Stimulus length (ms)');
+%     ylabel('Mask contrast at just noticeable difference (% luminance)');
 %     drawPublishAxis
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% create dots for optic flow
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function dots = initDots(dots,~)
-
-% maximum depth of points
-dots.dir = 0;
-
-area = (dots.maxX-dots.minX)*(dots.maxY-dots.minY);
-
-dots.n = area * dots.density;
-
-dots.x = rand(1,dots.n)*(dots.maxX-dots.minX)+dots.minX;
-dots.y = rand(1,dots.n)*abs(dots.maxY-dots.minY)+dots.minY;
-
-dots.x = dots.x;
-dots.y = dots.y;
-
-% set incoherent dots to 0
-dots.coherency = 1;
-dots.incoherent = rand(1,dots.n) > dots.coherency;
-dots.incoherentn = sum(dots.incoherent);
-dots.coherent = ~dots.incoherent;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% step dots for Radial
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function dots = updateDots(dots,coherence,myscreen,repick)
-
-% get the coherent and incoherent dots
-if coherence==1
-    dots.coherent = true(size(dots.x));
-    dots.incoherent = false(size(dots.x));
-    dots.incoherentn = 0;
-elseif repick
-    dots.incoherent = rand(1,dots.n) > coherence;
-    dots.incoherentn = sum(dots.incoherent);
-    dots.coherent = ~dots.incoherent;
-    dots.coherency = coherence;
-elseif dots.coherency ~= coherence
-    cohDiff = coherence - dots.coherency;
-    numDots = round(abs(cohDiff) * dots.n); % actual number of dots to flip
-    if numDots > dots.n, numDots = dots.n; end
-    if cohDiff > 0
-        % we need to add more coherent dots
-        flipDots = [zeros(1,numDots) ones(1,sum(dots.incoherent)-numDots)];
-        dots.incoherent(dots.incoherent) = flipDots(randperm(length(flipDots)));
-    else
-        % we need to add more incoherent dots
-        flipDots = [ones(1,numDots) zeros(1,sum(dots.coherent)-numDots)];
-        dots.incoherent(dots.coherent) = flipDots(randperm(length(flipDots)));
-    end
-    dots.incoherentn = sum(dots.incoherent);
-    dots.coherent = ~dots.incoherent;
-    dots.coherency = sum(dots.coherent)/dots.n;
-end
-dots.coherentn = dots.n-dots.incoherentn;
-
-freq_factor = dots.speed/myscreen.framesPerSecond;
-
-if dots.coherentn>0
-    % move coherent dots
-    % dots.dir is an angle, so compute x/y based on this
-    dots.y(dots.coherent) = dots.y(dots.coherent) + (freq_factor * sin(dots.dir));
-    dots.x(dots.coherent) = dots.x(dots.coherent) + (freq_factor * cos(dots.dir));
-end
-
-if dots.incoherentn>0
-    % these are for flipping into the other quadrants
-    rdir = rand(1,dots.incoherentn)*pi*2;
-    dots.y(dots.incoherent) = dots.y(dots.incoherent) + (freq_factor * sin(rdir));
-    dots.x(dots.incoherent) = dots.x(dots.incoherent) + (freq_factor * cos(rdir));
-end
-
-offscreen = dots.x > dots.maxX;
-dots.x(offscreen) = dots.x(offscreen) - abs(dots.maxX - dots.minX);
-offscreen = dots.x < dots.minX;
-dots.x(offscreen) = dots.x(offscreen) + abs(dots.maxX - dots.minX);
-
-offscreen = dots.y > dots.maxY;
-dots.y(offscreen) = dots.y(offscreen) - abs(dots.maxY - dots.minY);
-offscreen = dots.y < dots.minY;
-dots.y(offscreen) = dots.y(offscreen) + abs(dots.maxY - dots.minY);
-
-
-function setGT(myscreen,stimulus)
-
-% multipliers relative
-max = 95;
-% load the calibration
-load(myscreen.calibFullFilename);
-
-% get the max value from the calibration file
-localMax = calib.tableCorrected.luminance(end);
-
-% get an approximate factor
-factor = round(localMax/max);
-
-disp(sprintf('Setting gamma table to use %02.2f%% of available space.',1/factor*100));
-% if not 1, set the gammaTable accordingly
-
-if factor > 1
-    newTable = interp1(linspace(0,1,256),stimulus.linearizedGammaTable.redTable,linspace(0,1/factor,256));
-    succ = mglSetGammaTable(repmat(newTable',1,3));
-
-    if ~succ
-        warning('Gamma table set failure');
-        keyboard
-    end
-end
-
-function stimulus = initGratings(stimulus,myscreen)
-
-stimulus.maxIndex = 255;
-disppercent(-inf,'Creating grating textures');
-
-nContrasts = length(stimulus.contrast);
-nPhases = length(stimulus.grating.phases);
-
-gaussianWin = mglMakeGaussian(stimulus.grating.width,stimulus.grating.height,stimulus.grating.sdx,stimulus.grating.sdy);
-if strcmp(stimulus.grating.windowType,'gabor')
-  % a gaussian window
-  win = stimulus.maxIndex-stimulus.maxIndex*gaussianWin;
-else
-  % a simple window
-  win = stimulus.maxIndex-stimulus.maxIndex*(gaussianWin>exp(-1/2));
-end
-mask = ones(size(win,1),size(win,2),4)*1;%myscreen.grayIndex;
-win = win / max(win(:)) * 255;
-mask(:,:,4) = round(win);
-stimulus.mask = mglCreateTexture(mask);
-
-% make each one of he called for gratings
-for iPhase = 1:nPhases
-  for iContrast = 1:nContrasts
-    disppercent(calcPercentDone(iPhase,nPhases,iContrast,nContrasts));
-    % get the phase and contast
-    thisPhase = (stimulus.grating.phase+stimulus.grating.phases(iPhase))*180/pi;
-    thisContrast = stimulus.contrastOverride;
-    % make the grating
-    thisGrating = round(stimulus.maxIndex*((thisContrast*mglMakeGrating(stimulus.grating.width,nan,stimulus.grating.sf,0,thisPhase))+1)/2);
-    % create the texture
-%     thisGrating = repmat(thisGrating,453,1);
-    
-    stimulus.tex(iContrast,iPhase) = mglCreateTexture(thisGrating);
-  end
-end
-disppercent(inf);
-% stimulus.randMaskSize = [size(mask,1) size(mask,2)];
-% stimulus.randMask = mglCreateTexture(floor(stimulus.maxIndex*rand(stimulus.randMaskSize)));
-
-
-for iAngle = 1:length(stimulus.orientations)
-  % get center of patch
-  thisAngle = stimulus.orientations(iAngle);
-  centerX = stimulus.x + stimulus.grating.radius*cos(pi*thisAngle/180);
-  centerY = stimulus.y + stimulus.grating.radius*sin(pi*thisAngle/180);
-  % now get top and bottom point of grating
-  thisOrientation = thisAngle+90;
-  radius = sqrt((stimulus.grating.width/2).^2 +(stimulus.grating.height/2).^2)-0.5;
-  topX = centerX + radius*cos(pi*thisOrientation/180);
-  topY = centerY + radius*sin(pi*thisOrientation/180);
-  thisOrientation = thisOrientation+180;
-  bottomX = centerX + radius*cos(pi*thisOrientation/180);
-  bottomY = centerY + radius*sin(pi*thisOrientation/180);
-  % place points
-  stimulus.grating.refPoints.x{iAngle} = [topX bottomX];
-  stimulus.grating.refPoints.y{iAngle} = [topY bottomY];
-end
-
-stimulus.waitForBacktickText = mglText('Hit backtick (`) key to start');
+% elseif stimulus.localizer
+% else
+% %     perf = zeros(size(stimulus.istaircase));
+% %     for i = 1:length(stimulus.istaircase)
+% %         perf(i) = mean(stimulus.istaircase(i).response);
+% %     end
+% %     figure
+% %     plot(1:length(perf),perf,'o','MarkerFaceColor',[0 0 0],'MarkerEdgeColor',[1 1 1]);
+% %     set(gca,'XAxisTick',1:length(perf));
+% %     drawPublishAxis
+% end
