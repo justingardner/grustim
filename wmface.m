3b422a5e463e177be5bffb12df1426664e3718bd
Justin Gardner <jlg@stanford.edu>
2019-11-19 17:30:59 -0800

    Starting ability to do task as auditory only - not working yet, but has switches in to switch betwen visual and auditory

--- wmface.m
+++ wmface.m
@@ -6,83 +6,120 @@
 %       date: 10/15/2019
 %    purpose: working memory task based on Harrison & Tong with camera capture for Druckmann lab
 %
-%    Subjects are shown two  visual orientations and asked to remember either the first or the second.
-%    After a waiting period, subject is shown a third gradient and asked if it was rotated clockwise (1)
-%    or counterclockwise (2) from the remembered stimulus.
+%             Has both an auditory and visual version. Default is to run the auditory experiment,
+%             but this can be swapped by setting the taskType
+% 
+%             wmface('taskType=visual');
 %
-
-function e = wmface(scan)
+%             For the auditory task, two tones are presented and then the word one or two.
+%             Subject is instructed to remember the first or second tone depending on the
+%             spoken cue. After a delay period, the subject will hear a third tone and
+%             report whether the third tone is higher (button 1) or lower (button 2) then the
+%             remembered tone.
+%
+%             For the visual task, subjects are shown two gratings of different orientations in
+%             quick succession followed by the number 1 or 2 which indicates which grating orientation
+%             the subject should remember. After a delay period, the subject is shown a third grating
+%             and asked to report if it was rotated clockwise (button 1) or counterclockwise (button 2)
+%             from the remembered stimulus.
+%
+%             Other arguments:
+%
+%             scan=0: Set to 1 to add a synchToVol at end of trial
+%             delayInterval=5: Set to the desired delay interval in seconds
+%             useCamera=1: Set to 0 to not use camera (useful for debugging w/out camera connected)
+%             
+%
+function e = wmface(varargin)
 
 % check arguments
-if ~any(nargin == [0 1])
-  help cameraTest
-  return
-end
-
+getArgs(varargin,{'scan=0','taskType=auditory','delayInterval=5','useCamera=1','loFreq=440','hiFreq=560'});
 % default to non-scan mode
 if nargin < 1,scan = 0;end
 
-debugMode = 0;
-
-% stimulus parameters
+% global variable containing stimulus information
 global stimulus;
-stimulus.scan = scan;
-if stimulus.scan
-    waitForBacktick = 1;
-else
-    waitForBacktick = 0;
-end
-if scan
-    stimulus.contrast = 0.175;
-else
-    stimulus.contrast = 0.1;
-end
-stimulus.innerWidth = 1.5;
-stimulus.outerWidth = 10;
-stimulus.sf = 1;
-% set to false for sharp edged stimulus
-stimulus.gabor = 0;
-% fixation width
-stimulus.fixWidth = 1.5;
 
-% orientations to display
-stimulus.orientations = [25 115];
-stimulus.orientationJitter = 3;
-stimulus.constantVals = [3 6];
+% special settings for scanner
+stimulus.scan = scan;
+if stimulus.scan,waitForBacktick = 1;else, waitForBacktick = 0;end
+
+% check taskType and set settings appropriately
+switch lower(taskType)
+ case 'auditory'
+  % set auditory type
+  stimulus.taskType = lower(taskType);
+
+  % set auditory stimulus parameters
+  stimulus.baseFreq = [loFreq hiFreq];
+
+  % set background color
+  myscreen.background = 0;
+ case 'visual'
+  % set visual type
+  stimulus.taskType = lower(taskType);
+
+  % set the contrast higher for in the scanner
+  if scan,stimulus.contrast = 0.175;else, stimulus.contrast = 0.1;end
+  
+  % size of grating stimuli
+  stimulus.innerWidth = 1.5;
+  stimulus.outerWidth = 10;
+
+  % spatial frequency of grating
+  stimulus.sf = 1;
+
+  % set to false for sharp edged stimulus
+  stimulus.gabor = 0;
+
+  % fixation width
+  stimulus.fixWidth = 1.5;
+
+  % orientations to display
+  stimulus.orientations = [25 115];
+  stimulus.orientationJitter = 3;
+  stimulus.constantVals = [3 6];
+  
+  % set background color
+  myscreen.background = 128/255;
+ otherwise
+  disp(sprintf('(wmface) Unknown taskType: %s',taskType));
+  return
+end
 
 % delay interval in seconds
-stimulus.delayInterval = 11;
+stimulus.delayInterval = delayInterval;
 
-
-if debugMode
-  stimulus.delayInterval = 5;
-%   stimulus.constantVals = [20 30];
-%   stimulus.constantVals = 25;
-end
 % initalize the screen
-myscreen.background = 128/255;
 myscreen = initScreen(myscreen);
 
 % intialize the camera
-dispHeader('(cameraTest) Starting Camera Thread');
-tf = mglCameraThread('init');
-dispHeader;
-if ~tf
-  endScreen(myscreen);
-  dispHeader('(wmface) Could not initialize camera');
-  return
-end
-stimulus.cameraImages = {};
+stimulus.useCamera = useCamera;
+if stimulus.useCamera
+  dispHeader('(cameraTest) Starting Camera Thread');
+  tf = mglCameraThread('init');
+  dispHeader;
+  if ~tf
+    endScreen(myscreen);
+    dispHeader('(wmface) Could not initialize camera');
+    return
+  end
+  stimulus.cameraImages = {};
 
-% place to save data
-stimulus.cameraDataDir = myscreen.datadir;
-if ~isdir(stimulus.cameraDataDir),mkdir(stimulus.cameraDataDir);end
-if isempty(myscreen.SID)
-  stimulus.cameraFileStem = 'TEST';
+  % place to save data
+  stimulus.cameraDataDir = myscreen.datadir;
+  if ~isdir(stimulus.cameraDataDir),mkdir(stimulus.cameraDataDir);end
+  if isempty(myscreen.SID)
+    stimulus.cameraFileStem = 'TEST';
+  else
+    stimulus.cameraFileStem = sprintf('%s',myscreen.SID);
+  end
+  stimulus.cameraFileStem = sprintf('%s_%s_%s',stimulus.cameraFileStem,datestr(now,'yyyymmdd'),datestr(now,'HHMMSS'));
 else
-  stimulus.cameraFileStem = sprintf('%s',myscreen.SID);
-end
-stimulus.cameraFileStem = sprintf('%s_%s_%s',stimulus.cameraFileStem,datestr(now,'yyyymmdd'),datestr(now,'HHMMSS'));
+  dispHeader;
+  dispHeader('(wmface) Not using camera: useCamera set to 0');
+  dispHeader;
+end  
 
 % by waiting for the backtick key to be pressed before starting the experiment
 % (for systems that use NI digital I/O, this will wait for the digital
@@ -134,9 +171,11 @@ end
 myscreen = endTask(myscreen,task);
 
 % quit camera
-dispHeader('(cameraTest) Ending Camera Thread');
-mglCameraThread('quit');
-dispHeader;
+if stimulus.useCamera
+  dispHeader('(cameraTest) Ending Camera Thread');
+  mglCameraThread('quit');
+  dispHeader;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % function that gets called at the start of each segment
@@ -147,7 +186,9 @@ global stimulus;
 % set fixation white
 if task.thistrial.thisseg == 1
   % start saving camera
-  mglCameraThread('capture','timeToCapture',sum(task.segmax(1:(end-3))));
+  if stimulus.useCamera
+    mglCameraThread('capture','timeToCapture',sum(task.segmax(1:(end-3))));
+  end
   
   stimulus.fixColor = [1 1 1];
 
@@ -174,20 +215,25 @@ if task.thistrial.thisseg == 1
 elseif task.thistrial.thisseg == 9
   stimulus.fixColor = [1 1 1];
 elseif task.thistrial.thisseg == 10
-  % create filename for images
-  stimulus.saveCameraTime = mglGetSecs;
-  filename = fullfile(stimulus.cameraDataDir,sprintf('%s-%04i',stimulus.cameraFileStem,task.trialnum));
-  % tell thread to save images
-  stimulus.cameraImages{end+1} = mglCameraThread('save','videoFilename',filename);
-  % now save the task variables (just in case we crash)
-  save(fullfile(stimulus.cameraDataDir,stimulus.cameraFileStem),'myscreen','task','stimulus');
-  disp(sprintf('(wmface) Setting to save and task variable save took: %0.2fs',mglGetSecs(stimulus.saveCameraTime)));
+  if stimulus.useCamera
+    % create filename for images
+    stimulus.saveCameraTime = mglGetSecs;
+    filename = fullfile(stimulus.cameraDataDir,sprintf('%s-%04i',stimulus.cameraFileStem,task.trialnum));
+    % tell thread to save images
+    stimulus.cameraImages{end+1} = mglCameraThread('save','videoFilename',filename);
+    % now save the task variables (just in case we crash)
+    save(fullfile(stimulus.cameraDataDir,stimulus.cameraFileStem),'myscreen','task','stimulus');
+    disp(sprintf('(wmface) Setting to save and task variable save took: %0.2fs',mglGetSecs(stimulus.saveCameraTime)));
+  end
 elseif task.thistrial.thisseg == 11
-  % wait till camera is done
-  mglCameraThread('blockTillDone');
-  % display how long it took
-  disp(sprintf('(wmface) Save of camera files took %0.2fs',mglGetSecs(stimulus.saveCameraTime)));
-  % then jump to next segment (effectively end of trial)
+  if stimulus.useCamera
+    % wait till camera is done
+    mglCameraThread('blockTillDone');
+    % display how long it took
+    disp(sprintf('(wmface) Save of camera files took %0.2fs',mglGetSecs(stimulus.saveCameraTime)));
+    % then jump to next segment (effectively end of trial)
+  end
+  % end the segment
   task = jumpSegment(task);
 end
 
@@ -246,27 +292,32 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function stimulus = myInitStimulus(stimulus,myscreen)
 
-% compute the grating
-grating = mglMakeGrating(stimulus.outerWidth,stimulus.outerWidth,stimulus.sf,0,0);
+% setup visual stimulus
+if strcmp(stimulus.taskType,'visual')
 
-[gaussian xMesh yMesh] = mglMakeGaussian(stimulus.outerWidth,stimulus.outerWidth,stimulus.outerWidth/8,stimulus.outerWidth/8); 
-% calculate radius of each point
-r = sqrt(xMesh.^2 + yMesh.^2);
-% now calculate gaussian centered on width of stimulus
-gaussianRing = exp(-((r-(stimulus.outerWidth/4)).^2)/(stimulus.outerWidth/10)^2);
-gaussianRing(gaussianRing < 0.01) = 0;
-% hard edge
-if ~stimulus.gabor
+  % compute the grating
+  grating = mglMakeGrating(stimulus.outerWidth,stimulus.outerWidth,stimulus.sf,0,0);
+  [gaussian xMesh yMesh] = mglMakeGaussian(stimulus.outerWidth,stimulus.outerWidth,stimulus.outerWidth/8,stimulus.outerWidth/8); 
+  % calculate radius of each point
+  r = sqrt(xMesh.^2 + yMesh.^2);
+  % now calculate gaussian centered on width of stimulus
+  gaussianRing = exp(-((r-(stimulus.outerWidth/4)).^2)/(stimulus.outerWidth/10)^2);
+  gaussianRing(gaussianRing < 0.01) = 0;
+  % hard edge
+  if ~stimulus.gabor
     gaussianRing(gaussianRing>0) = 1;
     gaussianRing(r<stimulus.innerWidth/2) = 0;
+  end
+  gabor = round(255*(stimulus.contrast*grating.*gaussianRing+1)/2);
+  stimulus.tex = mglCreateTexture(gabor);
+
+  % make text
+  mglTextSet('Helvetica',32,[1 1 1],0,0,0);
+  stimulus.text(1) = mglText('1');
+  stimulus.text(2) = mglText('2');
+% setup auditory stimulus
+else
+  keyboard
 end
-gabor = round(255*(stimulus.contrast*grating.*gaussianRing+1)/2);
-stimulus.tex = mglCreateTexture(gabor);
-
-% make text
-mglTextSet('Helvetica',32,[1 1 1],0,0,0);
-stimulus.text(1) = mglText('1');
-stimulus.text(2) = mglText('2');
-
 % start staircase
 stimulus.s = doStaircase('init','fixed','fixedVals',stimulus.constantVals);
