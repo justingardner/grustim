89eb1eddf2a8fa4ec8f6f0b97294967f458a796f
Dan Birman <danbirman@gmail.com>
2020-02-12 20:15:50 -0800

    Starting to update spatobj for Kai's human experiment

--- spatobj.m
+++ spatobj.m
@@ -9,16 +9,7 @@ function [ myscreen ] = spatobj( varargin )
 % REFERENCES
 % Lindsay, G. W., & Miller, K. D. (2018). How biological attention mechanisms improve task performance in a large-scale visual system model. eLife, 7, e38105.
 % 
-% EXPERIMENT CALL:
-% scanning
-% afcom('scan=1');
-% TESTING CALL:
-% afcom('cue=#','noeye=1','powerwheel=0');
 
-%% add npy-matlab
-if isempty(which('readNPY'))
-    addpath(genpath('~/proj/npy-matlab'));
-end
 
 %% Over-write the stimulus struct
 
@@ -32,31 +23,59 @@ noeye = 0;
 eyewindow=0; 
 rebuild = 0;
 
-getArgs(varargin,{'scan=0','plots=0','noeye=0','eyewindow=2','rebuild=1'});
+getArgs(varargin,{'plots=0','noeye=0','eyewindow=2','rebuild=1'});
 stimulus.plots = plots;
 stimulus.noeye = noeye;
 stimulus.eyewindow = eyewindow;
 stimulus.remainCategory = 1:20;
 stimulus.rebuild = rebuild;
+stimulus.categories = {'artichoke','bathtub','cabbage butterfly','computer','mortar','greenhouse','padlock','toaster','paintbrush','football helmet','horned beetle','home theatre','stone wall','baked goods','coffee','spider','banana','clock','ferris wheel','seashore'};
 
 clear plots noeye eyewindow load
+
+%% Load iamges
+
+if stimulus.rebuild
+    % load data images
+    data = load('~/data/spatobj/stimuli_uint8.mat');
+    data = data.data;
+    
+    metaData = zeros(size(data,1),3);
+    metaData(:,1) = 1:size(metaData,1);
+    temp = repmat(1:20,80,1);
+    metaData(:,2) = temp(:);
+    temp = repmat([zeros(1,40) ones(1,40)]',1,20);
+    metaData(:,3) = temp(:);
+    % meta data columns
+    %   1        2               3
+    % idx  target categ  distractors_only
+    
+    % load data csv
+    
+    % NOTE: for now don't load the headers or data, because the data
+    % contain strings and csvreadh can't acutally handle that
+    %[metaHeader, metaData] = csvreadh('~/data/spatobj/stimuli_meta.csv');
+    %[exHeader, exData] = csvreadh('~/data/spatobj/exemplar_meta.csv');
+    
+    examples = load('~/data/spatobj/exemplars_uint8.mat');
+    examples = examples.data;
+
+    exData = zeros(size(examples,1),2);
+    % exData columns
+    %   1        2
+    % idx    category
+    exData(:,1) = 1:size(exData,1);
+    temp = repmat(1:20,5,1);
+    exData(:,2) = temp(:);
+end
 
 %% Open Old Stimfile
 
 if ~stimulus.rebuild && ~isempty(mglGetSID) && isdir(sprintf('~/data/spatobj/%s',mglGetSID))
-    % Directory exists, check for a stimefile
+    % Directory exists, check for a stimfile
     files = dir(sprintf('~/data/spatobj/%s/1*mat',mglGetSID));
 
     if length(files) >= 1
-        fname = files(1).name;
-        s = load(sprintf('~/data/spatobj/%s/%s',mglGetSID,fname));
-        stimulus.images = s.stimulus.images;
-        stimulus.categories = s.stimulus.categories;
-        stimulus.images_info = s.stimulus.images_info;
-        stimulus.distractors = s.stimulus.distractors;
-        stimulus.distractors_info = s.stimulus.distractors_info;
-        stimulus.imagesLoaded = s.stimulus.imagesLoaded;
-
         fname = files(end).name;
         s = load(sprintf('~/data/spatobj/%s/%s',mglGetSID,fname));
         % copy staircases and run numbers
@@ -77,10 +96,11 @@ myscreen.background = 0;
 stimulus.eyeFrames = myscreen.framesPerSecond * 0.300; % eye movements occur when for 300 ms someone moves out of the fixation region
 
 %% Reset the gamma table to be blank:
-gammaTable(:,1) = (0:1/255:1);
-gammaTable(:,2) = (0:1/255:1);
-gammaTable(:,3) = (0:1/255:1);
-mglSetGammaTable(gammaTable);
+% should this even happen?
+% gammaTable(:,1) = (0:1/255:1);
+% gammaTable(:,2) = (0:1/255:1);
+% gammaTable(:,3) = (0:1/255:1);
+% mglSetGammaTable(gammaTable);
 %% Sizes
 stimulus.arrayWidth = round(7*myscreen.screenWidth/myscreen.imageWidth); % in pixels
 disp(sprintf('Images will be resized to %1.2f pixels to match 7 degrees',stimulus.arrayWidth));
@@ -370,136 +390,3 @@ end
 if task.thistrial.thisseg<stimulus.seg.resp
     task = jumpSegment(task,stimulus.seg.resp);
 end
-        
-function loadStimulus()
-
-global stimulus
-
-% load the file which tracks which images are shown to which participants
-trackFile = '~/data/spatobj/tracking.mat';
-if isfile(trackFile)
-    track = load(trackFile);
-    track = track.track;
-else
-    track = struct;
-    track.todo = 1:95;
-    track.done = {};
-end
-% mygroup is the number from 1:95 which corresponds to the images which
-% will be shown to this participant, in theory if we run for all 95 groups
-% we can match the dataset with the CNN dataset
-mygroup = randsample(track.todo,1);
-track.todo = setdiff(track.todo,mygroup);
-save(trackFile,'track');
-
-imsz = stimulus.arrayWidth;
-% for each category we will show all 15 examples once, and we will also
-% show one random image from a different category. This means we need to
-% load at least 15 extra random images from each category, although they
-% won't all get used. 
-aimages = zeros(20,15,imsz,imsz,3,'uint8');
-aimages_info = zeros(20,15,4,'uint8');
-
-adistractors = zeros(20,15,imsz,imsz,3,'uint8');
-adistractors_info = zeros(20,15,4,'uint8');
-
-%% distractors
-% the first thing to do is load all of the info files, and concatenate them
-% into one data array. We need to do this to search for distractor images
-% which don't include each of the categories. 
-ainfo = nan(20*15*20,7);
-count = 1;
-
-for cat = 0:19
-    info = readNPY(sprintf('~/data/spatobj/images/dat%i_info.npy',cat));
-    
-    for ci = 1:15
-        for gi = 1:95
-            ainfo(count,:) = [cat squeeze(info(ci,gi,:))' ci gi];
-            count = count+1;
-        end
-    end
-end
-clear info
-
-% now we will go through each category (1:20) and pick 15 distractor images
-% from the other categories which don't have this category in them
-distractorList = zeros(20,15,2); % this will be the category # and group # of the distractor image
-
-for cat = 0:19
-    % remove all rows which include this category
-    info = ainfo(~any(ainfo(:,1:5)==cat,2),:);
-    % pick 15 random categories
-    myDistCats = randsample(setdiff(0:19,cat),15);
-    % for each category, pick a random group from the available options
-    for di = 1:length(myDistCats)
-        dcat = myDistCats(di);
-        % check which groups are available
-        cinfo = info(info(:,1)==dcat,:);
-        % get 15 random images
-        idxs = randsample(1:size(cinfo,1),1);
-        % save these
-        distractorList(cat+1,di,:) = cinfo(idxs,6:7);
-    end
-end
-
-%% now load the actual images
-
-disppercent(-1/20);
-for cat = 0:19
-    dat = readNPY(sprintf('~/data/spatobj/images/dat%i.npy',cat));
-    info = readNPY(sprintf('~/data/spatobj/images/dat%i_info.npy',cat));
-    
-    % get all the images for my group
-    images = squeeze(dat(:,mygroup,:,:,:));
-    images_info = squeeze(info(:,mygroup,:));
-    
-    % resize images
-    images_sz = zeros(size(images,1),imsz,imsz,3);
-    for i = 1:size(images,1)
-        images_sz(i,:,:,:) = imresize(imrotate(squeeze(images(i,:,:,:)),-90),[imsz imsz]);
-    end
-    
-    aimages(cat+1,:,:,:,:) = uint8(images_sz);
-    aimages_info(cat+1,:,:) = uint8(images_info);
-    
-    % go through the distractors and pull any images that are needed
-    for dc = 0:19
-        for di = 1:15
-            if distractorList(dc+1,di,1)==cat
-                temp = dat(distractorList(dc+1,di,1),distractorList(dc+1,di,2),:,:,:);
-                adistractors(dc+1,di,:,:,:) = uint8(imresize(imrotate(squeeze(temp),-90),[imsz imsz]));
-                adistractors_info(dc+1,di,:) = info(distractorList(dc+1,di,1),distractorList(dc+1,di,2),:);
-            end
-        end
-    end
-    
-    disppercent(cat/20);
-    
-    clear dat
-    clear info
-end
-disppercent(inf);
-
-%% and save
-stimulus.categories = {'paintbrush','wall clock','seashore','paddlewheel','padlock','garden spider','long-horned beetle','cabbage butterfly','toaster','greenhouse','bakery','stone wall','artichoke','modem','football helmet','stage','mortar','soup','dough','bathtub'};
-stimulus.imagesLoaded = true;
-stimulus.images = aimages;
-stimulus.images_info = aimages_info;
-stimulus.distractors = adistractors;
-stimulus.distractors_info = adistractors_info;
-
-%% test code (to check that images actually have their category)
-% figure;`2221211221212111122112121222
-% for cat = 1:20
-%     % pick a random image
-%     idx = randi(15);
-%     % display
-%     imagesc(squeeze(aimages(cat,idx,:,:,:)));
-%     % tell me what this is
-%     r = input(sprintf('Press [enter] if you see a %s',stimulus.categories{cat}),'s');
-%     if ~isempty(r)
-%         disp(aimages_info(cat,idx,:));
-%         warning(sprintf('Category %i index %i',cat,idx));
-%     end
-% end
