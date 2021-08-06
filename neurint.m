85bdb58e69e81f9156c6d5cabcb7d9147740100e
Akshay Jagadeesh <akjags@gmail.com>
2021-08-06 16:40:42 -0700

    neurint

--- neurint.m
+++ neurint.m
@@ -1,8 +1,8 @@
-function [ myscreen ] = neurint( varargin )
+function [ myscreen ] = neurint(varargin)
 %
 % Neural Interpolations
-%  How well can observers discriminate along an arbitrary axis through
-%  IT representational space?
+%  How well can observers discriminate along a specific dimension through
+%  V4, IT or dCNN representational space?
 %
 %  Usage: neurint(varargin)
 %  Authors: Akshay Jagadeesh
@@ -20,7 +20,7 @@ scan = 1;
 getArgs(varargin,{'scan=0', 'testing=0'}, 'verbose=1');
 stimulus.scan = scan;
 stimulus.debug = testing;
-clear scan testing sType;
+clear scan testing;
 
 %% Stimulus parameters 
 %% Open Old Stimfile
@@ -63,30 +63,38 @@ stimulus.live.cueColor = stimulus.colors.black;
 stimulus.curTrial(1) = 0;
 
 % Task important variables
-stimulus.image1_name = 'elephant1';
-stimulus.image2_name = 'face1';
 stimulus.intervals = [0, 25, 50, 75, 100];
 stimulus.imSize = 12;
-stimulus.stimXPos = 13;
+stimulus.stimXPos = 8;
 stimulus.num_samples = 4;
+stimulus.feature_spaces = {'pool3-V1', 'pool4-IT'};
+stimulus.image_pairs = {'elephant1_face1', 'horse_cat', 'rocks_leaves'};
 
 %% Select the condition for this run
 % Choose which image and which pooling layer to display on this run on each side
 stimulus.stimDir = '~/proj/NeuralImageSynthesis/neurint/bw_new';
+stimulus.stimDir = '~/proj/neurint/outputs/bw';
 
 %% Preload images
-mask = imread('~/proj/TextureSynthesis/stimuli/Flattop8.tif');
+mask = imread('~/proj/NeuralImageSynthesis/Flattop8.tif');
 stimulus.images= struct();
 disppercent(-inf, 'Preloading images');
 
 % load texture and noise samples
-for i = 1:length(stimulus.intervals)
-  interval = stimulus.intervals(i);
-  for j = 1:stimulus.num_samples
-    image = imread(sprintf('%s/%s_%s_%i_pool4-IT_s%i.png', stimulus.stimDir, stimulus.image1_name, stimulus.image2_name, interval, j));
-    stimulus.images.(sprintf('interval%i_smp%i', interval, j)) = genTexFromIm(image, mask);
+for fi = 1:length(stimulus.feature_spaces)
+  for ii = 1:length(stimulus.image_pairs)
+    image_pair = stimulus.image_pairs{ii};
+    feature_space = strrep(stimulus.feature_spaces{fi}, '-', '_');
+    for i = 1:length(stimulus.intervals)
+      interval = stimulus.intervals(i);
+      for j = 1:stimulus.num_samples
+        image = imread(sprintf('%s/%s_%i_%s_s%i.png', stimulus.stimDir, image_pair, interval, stimulus.feature_spaces{fi}, j));
+        stimulus.images.(sprintf('%s_%s_interval%i_smp%i', image_pair, feature_space, interval, j)) = genTexFromIm(image, mask);
+      end
+    end
   end
-  disppercent(i / length(stimulus.intervals));
+  stimulus.feature_spaces{fi} = feature_space;
+  disppercent(fi / length(stimulus.feature_spaces));
 end
 disppercent(inf);
 clear image
@@ -94,20 +102,22 @@ clear image
 %%%%%%%%%%%%% TASK %%%%%%%%%%%%%%%%%
 task{1}{1} = struct;
 task{1}{1}.waitForBacktick = 1;
-task{1}{1}.segmin = [0.2, 5.0, 0.2];
-task{1}{1}.segmax = [0.2, 5.0, 0.2];
+task{1}{1}.segmin = [0.2, 0.2, 0.2, 5.0, 0.2];
+task{1}{1}.segmax = [0.2, 0.2, 0.2, 5.0, 0.2];
 
 stimulus.seg = {};
 stimulus.seg.fix = 1;
-stimulus.seg.stim = 2;
-stimulus.seg.feedback = 3;
+stimulus.seg.stim1 = 2;
+stimulus.seg.isi = 3;
+stimulus.seg.stim2 = 4;
+stimulus.seg.feedback = 5;
 
 % Trial parameters
 task{1}{1}.synchToVol = zeros(size(task{1}{1}.segmin));
 task{1}{1}.getResponse = zeros(size(task{1}{1}.segmin));
-task{1}{1}.getResponse(stimulus.seg.stim) = 1;
+task{1}{1}.getResponse(stimulus.seg.stim2) = 1;
 
-task{1}{1}.numTrials = 60;
+task{1}{1}.numTrials = 250;
 task{1}{1}.random = 1;
 
 if stimulus.scan
@@ -120,12 +130,19 @@ end
 % Initialize task parameters
 
 % Assign layer, texture family, and sample index in random blocks.
-task{1}{1}.parameter.leftSample = 1:stimulus.num_samples;
-task{1}{1}.parameter.rightSample = 1:stimulus.num_samples;
+task{1}{1}.parameter.img1Sample = 1:stimulus.num_samples;
+task{1}{1}.parameter.img2Sample = 1:stimulus.num_samples;
 task{1}{1}.parameter.centerInterval = stimulus.intervals;
+task{1}{1}.parameter.image_pair = 1:length(stimulus.image_pairs);
+task{1}{1}.parameter.leftImg = [1 2]; % 1 = img1, 2=img2;
+task{1}{1}.randVars.uniform.feature_space = 1:length(stimulus.feature_spaces);
 task{1}{1}.randVars.calculated.correct = NaN;
 task{1}{1}.randVars.calculated.response = NaN;
+task{1}{1}.randVars.calculated.imgChoice = NaN;
+task{1}{1}.randVars.calculated.rightImg = NaN;
 task{1}{1}.randVars.calculated.centerSample = NaN;
+task{1}{1}.randVars.calculated.leftSample = NaN;
+task{1}{1}.randVars.calculated.rightSample = NaN;
 
 for phaseNum = 1:length(task{1})
   [task{1}{phaseNum}, myscreen] = initTask(task{1}{phaseNum},myscreen,@startSegmentCallback,@screenUpdateCallback,@getResponseCallback,@startTrialCallback,[],[]);
@@ -185,19 +202,34 @@ global stimulus
 stimulus.live.gotResponse = 0;
 stimulus.curTrial(task.thistrial.thisphase) = stimulus.curTrial(task.thistrial.thisphase) + 1;
 
+feature_space = stimulus.feature_spaces{task.thistrial.feature_space};
+
 if task.thistrial.centerInterval == 0
-  task.thistrial.centerSample = randsample(setdiff(1:stimulus.num_samples, task.thistrial.leftSample), 1);
+  task.thistrial.centerSample = randsample(setdiff(1:stimulus.num_samples, task.thistrial.img1Sample), 1);
 elseif task.thistrial.centerInterval == 100
-  task.thistrial.centerSample = randsample(setdiff(1:stimulus.num_samples, task.thistrial.rightSample), 1);
+  task.thistrial.centerSample = randsample(setdiff(1:stimulus.num_samples, task.thistrial.img2Sample), 1);
 else
   task.thistrial.centerSample = randsample(1:stimulus.num_samples, 1);
 end
 
-stimulus.live.leftStim = stimulus.images.(sprintf('interval0_smp%i', task.thistrial.leftSample));
-stimulus.live.rightStim = stimulus.images.(sprintf('interval100_smp%i', task.thistrial.rightSample));
-stimulus.live.centerStim = stimulus.images.(sprintf('interval%i_smp%i', task.thistrial.centerInterval, task.thistrial.centerSample));
+task.thistrial.rightImg = setdiff(1:2, task.thistrial.leftImg);
+image_pair = stimulus.image_pairs{task.thistrial.image_pair};
 
-disp(sprintf('--- Trial %i - Left (0%%) Sample %i -- Center (%i%%), sample %i -- Right (100%%), sample %i ---', task.trialnum, task.thistrial.leftSample, task.thistrial.centerInterval, task.thistrial.centerSample, task.thistrial.rightSample));
+if task.thistrial.leftImg == 1
+  stimulus.live.leftStim = stimulus.images.(sprintf('%s_%s_interval0_smp%i', image_pair, feature_space, task.thistrial.img1Sample));
+  stimulus.live.rightStim = stimulus.images.(sprintf('%s_%s_interval100_smp%i', image_pair, feature_space, task.thistrial.img2Sample));
+  task.thistrial.leftSample = task.thistrial.img1Sample;
+  task.thistrial.rightSample = task.thistrial.img2Sample;
+  disp(sprintf('--- Trial %i - Left (0%%) Sample %i -- Center (%i%%), sample %i -- Right (100%%), sample %i ---', task.trialnum, task.thistrial.leftSample, task.thistrial.centerInterval, task.thistrial.centerSample, task.thistrial.rightSample));
+else
+  stimulus.live.leftStim = stimulus.images.(sprintf('%s_%s_interval100_smp%i', image_pair, feature_space, task.thistrial.img2Sample));
+  stimulus.live.rightStim = stimulus.images.(sprintf('%s_%s_interval0_smp%i', image_pair, feature_space, task.thistrial.img1Sample));
+  task.thistrial.leftSample = task.thistrial.img2Sample;
+  task.thistrial.rightSample = task.thistrial.img1Sample;
+  disp(sprintf('--- Trial %i - Left (100%%) Sample %i -- Center (%i%%), sample %i -- Right (0%%), sample %i ---', task.trialnum, task.thistrial.leftSample, task.thistrial.centerInterval, task.thistrial.centerSample, task.thistrial.rightSample));
+end
+stimulus.live.centerStim = stimulus.images.(sprintf('%s_%s_interval%i_smp%i', image_pair, feature_space, task.thistrial.centerInterval, task.thistrial.centerSample));
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% Runs at the start of each Segment %%%%%%%%%%%%%%%%
@@ -208,6 +240,7 @@ global stimulus
 
 % Save segment start time;
 task.thistrial.tSegStart(task.thistrial.thisseg) = mglGetSecs;
+stimulus.live.screenGrab = 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% Refreshes the Screen %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -218,11 +251,16 @@ function [task, myscreen] = screenUpdateCallback(task, myscreen)
 global stimulus
 mglClearScreen(0.5);
 
-if task.thistrial.thisseg == stimulus.seg.stim
+if task.thistrial.thisseg == stimulus.seg.stim1
+  mglBltTexture(stimulus.live.centerStim, [0,0, stimulus.imSize, stimulus.imSize]);
+  if stimulus.live.screenGrab && stimulus.screenGrab
+      frame=mglScreenGrab();
+      
+  end
+elseif task.thistrial.thisseg == stimulus.seg.stim2
   mglBltTexture(stimulus.live.leftStim, [-stimulus.stimXPos, 0, stimulus.imSize, stimulus.imSize]);
   mglBltTexture(stimulus.live.rightStim, [stimulus.stimXPos, 0, stimulus.imSize, stimulus.imSize]);
-  mglBltTexture(stimulus.live.centerStim, [0,0, stimulus.imSize, stimulus.imSize]);
-end
+ end
 
 if task.thistrial.thisseg == stimulus.seg.feedback
   if ~isnan(task.thistrial.correct) && task.thistrial.correct==1
@@ -271,10 +309,12 @@ if validResponse
     task.thistrial.detected = 1;
     task.thistrial.response = task.thistrial.whichButton;
     disp(sprintf('Response: %i', task.thistrial.response));
-    if task.thistrial.response == 1
+    if (task.thistrial.response == 1 && task.thistrial.leftImg == 1) || (task.thistrial.response == 2 && task.thistrial.leftImg==2)
       task.thistrial.correct = task.thistrial.centerInterval < 50;
+      task.thistrial.imgChoice = 1;
     else
       task.thistrial.correct = task.thistrial.centerInterval >=50;
+      task.thistrial.imgChoice = 2;
     end
     if task.thistrial.correct
     	disp('Correct!')
