879e44188de8f81f25fe029243f01012e5d45bf8
Josh Wilson <joshwilson@DN0a237d42.SUNet>
2022-10-24 16:32:55 -0700

    plots the timing of magnet triggers

---
+++ checkTraceTiming.m
@@ -0,0 +1,34 @@
+%    checkTraceTiming.m
+%
+%       
+%       usage: checkTraceTiming
+%       by: josh wilson
+%       date: October 2022
+%       
+%       Takes a stim file and plots the timing of the triggers
+%      
+%       Example Usage:
+%           checkTraceTiming('stimFile=testTrigger')
+%           
+
+function checkTraceTiming(varargin)
+
+% get stimfile name and load
+getArgs(varargin);
+load(stimFile)
+
+%get the times
+traces = makeTraces(myscreen) 
+triggerTimes = myscreen.events.time(myscreen.events.tracenum == 1);
+triggerTimes = triggerTimes - triggerTimes(1); %subtract timing of first trigger
+triggerTimes = triggerTimes(2:end); %take out first backtick timing (experiment start)
+
+% plot the trigger times
+figure,
+subplot(1,2,1); scatter(1:length(triggerTimes),triggerTimes);
+hold on, plot([1 length(triggerTimes)], [triggerTimes(1) triggerTimes(end)]);
+xlabel('Trigger number');ylabel('Time (s)');
+
+% plot trigger times as difference from expect timing assuming all are equal
+subplot(1,2,2); scatter(1:length(triggerTimes),triggerTimes-((1:length(triggerTimes))*((triggerTimes(end)-triggerTimes(1))/length(triggerTimes))));
+xlabel('Trigger number');ylabel('Difference from expected time (s)');
