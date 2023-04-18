613df520db98ddb076723369aca6ede13e82d9e9
Fran Bottazzini <franbottazzini@DNa820b74.SUNet>
2023-04-17 19:21:25 -0700

    edit texobj

--- honors_thesis/fb_texobj.m
+++ honors_thesis/fb_texobj.m
@@ -60,7 +60,6 @@ stimulus.imageNames = {'adult-10', 'adult-11', 'adult-12', 'adult-13', 'adult-10
                     'adult-103', 'adult-104', 'adult-105', 'adult-106', 'adult-107', 'adult-108',...
                     'house-1', 'house-100', 'house-102','house-104', 'house-105', 'house-107', ...
                     'house-108','house-109', 'house-110', 'house-111','house-114', 'house-115',};
-stimulus.imageNames = {'bear', 'cat', 'horse', 'truck', 'face', 'helicopter', 'house', 'lawn', 'moss', 'dirt'};
 stimulus.layerNames = {'pool4'};
 stimulus.poolSize = '1x1_';
 
